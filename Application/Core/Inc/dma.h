8fc43c6830bc40f59ec49ea7322cad35fdefac34
Bùi Việt Đức <71390823+Sagit-ctrl@users.noreply.github.com>
2024-03-07 00:03:57 +0700

    Bùi Việt Đức
    Test

--- Application/Core/Inc/dma.h
+++ Application/Core/Inc/dma.h
@@ -27,16 +27,6 @@ extern "C" {
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* DMA memory to memory transfer handles -------------------------------------*/
-
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* USER CODE BEGIN Private defines */
-
-/* USER CODE END Private defines */
-
 void MX_DMA_Init(void);
 
 /* USER CODE BEGIN Prototypes */
